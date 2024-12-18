ca56cd51d06b4f3723f69da31a7e5ac31fdb7b03
JiksGit <2wlrlwkd@gmail.com>
2024-12-18 20:54:25 +0900

    Catalog-service commit

--- src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
+++ src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
@@ -1,7 +1,7 @@
-package com.example.catalogservice.entity;
+package com.example.catalogservice.jpa;
 
 import org.springframework.data.repository.CrudRepository;
 
 public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
-    
+    CatalogEntity findByProductId(String productId);
 }
